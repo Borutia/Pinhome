41fbf00cfe15e8c138c65d87351d048dd723695d
Dmitry Borutya <borutia@mail.ru>
2020-10-26 13:15:12 +0500

    save

--- settings/configuration.py
+++ settings/configuration.py
@@ -4,7 +4,7 @@ import os
 class Config:
     DEBUG = False
     TESTING = False
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://pinhouse:pinhouse@localhost:5433/pinhouse'
+    #SQLALCHEMY_DATABASE_URI = 'postgres://kshcgzkjxlfwdc:2a105932715ec1b6732b6ab9204b252544d03aaceff10005f28512a68e30a9f4@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/d7vc1htmtmq5fa'
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:2452020@localhost:5432/pinhome'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
