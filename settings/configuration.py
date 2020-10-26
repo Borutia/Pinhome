f0b28e01655e589acf964cae1680cd98a656542e
Dmitry Borutya <borutia@mail.ru>
2020-10-26 23:57:46 +0500

    save

--- settings/configuration.py
+++ settings/configuration.py
@@ -5,8 +5,8 @@ class Config:
     DEBUG = False
     TESTING = False
     #SQLALCHEMY_DATABASE_URI = 'postgres://kshcgzkjxlfwdc:2a105932715ec1b6732b6ab9204b252544d03aaceff10005f28512a68e30a9f4@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/d7vc1htmtmq5fa'
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:2452020@localhost:5432/pinhome'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:2452020@localhost:5432/pinhome'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
     PROJECT_HOME = os.path.dirname(os.path.realpath(__file__)).replace('/settings', '')
@@ -17,5 +17,9 @@ class Config:
         'facebook': {
             'id': '649881479030378',
             'secret': '32b05f9979c1a1bb7632e31f216e38cb'
+        },
+        'twitter': {
+            'id': '3RzWQclolxWZIMq5LJqzRZPTl',
+            'secret': 'm9TEd58DSEtRrZHpz2EjrV9AhsBRxKMo8m3kuIZj3zLwzwIimt'
         }
     }
